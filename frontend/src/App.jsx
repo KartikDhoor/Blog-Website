65cb16379159ab7285ac1d75c1156150cab1e8b3
KartikDhoor <127188028+KartikDhoor@users.noreply.github.com>
2025-01-15 19:24:04 +0530

    route setting
    setting app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Master from './component/Master'
 import Home from './component/pages/Home'
 import Blog from './component/pages/Blog'
-import Inspire from './component/pages/inspire'
+import Inspire from './component/pages/Inspire'
 import Contact from './component/pages/Contact'
 import News from './component/pages/News'
 
