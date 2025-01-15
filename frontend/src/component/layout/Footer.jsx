04a8a43cafae8d89e965928a1af0b92d98d6def2
KartikDhoor <127188028+KartikDhoor@users.noreply.github.com>
2025-01-16 00:43:31 +0530

    title footer
    make the title making the logo and the make the footer responsive

--- frontend/src/component/layout/Footer.jsx
+++ frontend/src/component/layout/Footer.jsx
@@ -8,48 +8,97 @@ export default function Footer() {
                 <div className="lg:h-[60vh] lg:w-full md:h-[60vh] md:w-full sm:h-full sm:w-full belowSm:h-full belowSm:w-full bg-dark1">
                     <div className="lg:h-full lg:w-[90%] md:h-full md:w-[90%] sm:h-full sm:w-[90%] belowSm:h-full belowSm:w-[90%] mx-auto p-4">
                         <div className="h-[30vh] w-full flex justify-center items-center">
-                            <div className="h-full w-[20%] flex items-center justify-center">
+                            <div className="lg:h-full lg:w-[20%] lg:flex lg:items-center lg:justify-center
+                                            md:h-full md:w-[20%] md:flex md:items-center md:justify-center
+                                            sm:h-full sm:w-[40%] sm:flex sm:items-center sm:justify-center
+                                            belowSm:h-full belowSm:w-[40%] belowSm:flex belowSm:items-center belowSm:justify-center">
                                 <GiRevolt className="text-9xl text-amber-400" />
                             </div>
-                            <div className="h-full w-[80%] flex items-center justify-center">
+                            <div className="lg:h-full lg:w-[80%] lg:flex lg:items-center lg:justify-center
+                                            md:h-full md:w-[80%] md:flex md:items-center md:justify-center
+                                            sm:h-full sm:w-[60%] sm:flex sm:items-center sm:justify-center
+                                            belowSm:h-full belowSm:w-[60%] belowSm:flex belowSm:items-center belowSm:justify-center">
                                 <div>
-                                <button className=" bg-gray1 px-2 border rounded border-gray-800">
-                                    <p className="text-white">Learn,Connect and innovate</p>
-                                </button>
-                                <p className="text-5xl text-white font-medium my-2 sm:hidden">Be part of the Future Tech Revloution</p>
-                                <p className="text-sm text-gray1 font-normal sm:hidden">immerse yourself in the world of future Technology.Explore Our comprehensive Resource,Connect with the fellow tech enthusiasts,and drive innovation in the industry.Join a Dynamic comunity of forward thinkers.</p>
+                                    <button className=" bg-gray1 px-2 border rounded border-gray-800">
+                                        <p className="text-white">Learn,Connect and innovate</p>
+                                    </button>
+                                    <p className="text-5xl text-white font-medium my-2 lg:block md:block sm:hidden belowSm:hidden">Be part of the Future Tech Revloution</p>
+                                    <p className="text-sm text-gray1 font-normal lg:block md:block sm:hidden belowSm:hidden">immerse yourself in the world of future Technology.Explore Our comprehensive Resource,Connect with the fellow tech enthusiasts,and drive innovation in the industry.Join a Dynamic comunity of forward thinkers.</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="h-[20vh] w-full p-2 rounded-lg border border-gray-800 flex justify-center gap-2 bg-pureblack">
-                            <div className="h-full w-[33%] p-2 rounded-lg border-gray-800 bg-dark2">
-                                <div className="h-[8vh] w-full mx-auto flex justify-center items-center">
-                                    <div className="h-full w-[80%] p-2">
+                        <div className="lg:hidden md:hidden sm:h-[30vh] sm:w-full belowSm:h-[40vh] belowSm:w-full">
+                            <p className="lg:hidden md:hidden text-5xl text-white font-medium my-2">Be part of the Future Tech Revloution</p>
+                            <p className="lg:hidden md:hidden text-sm text-gray1 font-normal">immerse yourself in the world of future Technology.Explore Our comprehensive Resource,Connect with the fellow tech enthusiasts,and drive innovation in the industry.Join a Dynamic comunity of forward thinkers.</p>
+
+                        </div>
+                        <div className="lg:h-[25vh] lg:w-full lg:rounded-lg lg:border lg:border-gray-800 lg:flex lg:justify-center lg:items-center lg:gap-2 lg:bg-pureblack
+                                        md:h-[25vh] md:w-full md:rounded-lg md:border md:border-gray-800 md:flex md:justify-center md:items-center md:gap-2 md:bg-pureblack
+                                        sm:h-full sm:w-full sm:p-2 sm:rounded-lg sm:border sm:border-gray-800 sm:bg-pureblack
+                                        belowSm:h-full belowSm:w-full belowSm:p-2 belowSm:rounded-lg belowSm:border belowSm:border-gray-800 belowSm:bg-pureblack
+                                        
+                                        ">
+                            <div className="lg:h-full lg:w-[33%] lg:p-2 lg:rounded-lg lg:border-gray-800 lg:bg-dark2
+                                            md:h-full md:w-[33%] md:p-2 md:rounded-lg md:border-gray-800 md:bg-dark2
+                                            sm:h-[20vh] sm:w-full sm:p-2 sm:rounded-lg sm:border-gray-800 sm:bg-dark2 sm:my-2
+                                            belowSm:h-[20vh] belowSm:w-full belowSm:p-2 belowSm:rounded-lg belowSm:border-gray-800 belowSm:bg-dark2 belowSm:my-2"
+                            >
+                                <div className="lg:h-[10vh] lg:w-full lg:mx-auto lg:flex lg:justify-center lg:items-center
+                                                md:h-[10vh] md:w-full md:mx-auto md:flex md:justify-center md:items-center
+                                                sm:h-[10vh] sm:w-full sm:mx-auto sm:flex sm:justify-center sm:items-center
+                                                belowSm:h-[10vh] belowSm:w-full belowSm:mx-auto belowSm:flex belowSm:justify-center belowSm:items-center
+                                                ">
+                                    <div className="lg:h-full lg:w-[80%] lg:p-2
+                                                    md:h-full md:w-[80%] md:p-2
+                                                    sm:h-full sm:w-[80%] sm:p-2
+                                                    belowSm:h-full belowSm:w-[80%] belowSm:p-2">
                                         <p className="text-white text-2xl font-medium">Resource Access</p>
                                     </div>
-                                    <div className="h-full w-[20%]">
+                                    <div className="lg:h-full lg:w-[20%] md:h-full md:w-[20%] sm:h-full sm:w-[20%] belowSm:h-full belowSm:w-[20%]">
                                         <BsArrowUpRightCircleFill className="text-5xl text-amber-400" />
                                     </div>
                                 </div>
                                 <p className="text-sm text-gray-400 font-normal">Visitor can access a wide range of reaource,including ebooks, WhitePapaer, reports.</p>
                             </div>
-                            <div className="h-full w-[33%] p-2 rounded-lg border-gray-800 bg-dark2">
-                                <div className="h-[8vh] w-full mx-auto flex justify-center items-center">
-                                    <div className="h-full w-[80%] p-2">
+                            <div className="lg:h-full lg:w-[33%] lg:p-2 lg:rounded-lg lg:border-gray-800 lg:bg-dark2
+                                            md:h-full md:w-[33%] md:p-2 md:rounded-lg md:border-gray-800 md:bg-dark2
+                                            sm:h-[20vh] sm:w-full sm:p-2 sm:rounded-lg sm:border-gray-800 sm:bg-dark2 sm:my-2
+                                            belowSm:h-[20vh] belowSm:w-full belowSm:p-2 belowSm:rounded-lg belowSm:border-gray-800 belowSm:bg-dark2 belowSm:my-2"
+                            >
+                                <div className="lg:h-[10vh] lg:w-full lg:mx-auto lg:flex lg:justify-center lg:items-center
+                                                md:h-[10vh] md:w-full md:mx-auto md:flex md:justify-center md:items-center
+                                                sm:h-[10vh] sm:w-full sm:mx-auto sm:flex sm:justify-center sm:items-center
+                                                belowSm:h-[10vh] belowSm:w-full belowSm:mx-auto belowSm:flex belowSm:justify-center belowSm:items-center
+                                                ">
+                                    <div className="lg:h-full lg:w-[80%] lg:p-2
+                                                    md:h-full md:w-[80%] md:p-2
+                                                    sm:h-full sm:w-[80%] sm:p-2
+                                                    belowSm:h-full belowSm:w-[80%] belowSm:p-2">
                                         <p className="text-white text-2xl font-medium">Comunity Forms</p>
                                     </div>
-                                    <div className="h-full w-[20%]">
+                                    <div className="lg:h-full lg:w-[20%] md:h-full md:w-[20%] sm:h-full sm:w-[20%] belowSm:h-full belowSm:w-[20%]">
                                         <BsArrowUpRightCircleFill className="text-5xl text-amber-400" />
                                     </div>
                                 </div>
                                 <p className="text-sm text-gray-400 font-normal">Visitor can access a wide range of reaource,including ebooks, WhitePapaer, reports.</p>
                             </div>
-                            <div className="h-full w-[33%] p-2 rounded-lg border-gray-800 bg-dark2">
-                                <div className="h-[8vh] w-full mx-auto flex justify-center items-center">
-                                    <div className="h-full w-[80%] p-2">
+                            <div className="lg:h-full lg:w-[33%] lg:p-2 lg:rounded-lg lg:border-gray-800 lg:bg-dark2
+                                            md:h-full md:w-[33%] md:p-2 md:rounded-lg md:border-gray-800 md:bg-dark2
+                                            sm:h-[20vh] sm:w-full sm:p-2 sm:rounded-lg sm:border-gray-800 sm:bg-dark2 sm:my-2
+                                            belowSm:h-[20vh] belowSm:w-full belowSm:p-2 belowSm:rounded-lg belowSm:border-gray-800 belowSm:bg-dark2 belowSm:my-2"
+                            >
+                                <div className="lg:h-[10vh] lg:w-full lg:mx-auto lg:flex lg:justify-center lg:items-center
+                                                md:h-[10vh] md:w-full md:mx-auto md:flex md:justify-center md:items-center
+                                                sm:h-[10vh] sm:w-full sm:mx-auto sm:flex sm:justify-center sm:items-center
+                                                belowSm:h-[10vh] belowSm:w-full belowSm:mx-auto belowSm:flex belowSm:justify-center belowSm:items-center
+                                                ">
+                                    <div className="lg:h-full lg:w-[80%] lg:p-2
+                                                    md:h-full md:w-[80%] md:p-2
+                                                    sm:h-full sm:w-[80%] sm:p-2
+                                                    belowSm:h-full belowSm:w-[80%] belowSm:p-2">
                                         <p className="text-white text-2xl font-medium">Teach Events</p>
                                     </div>
-                                    <div className="h-full w-[20%]">
+                                    <div className="lg:h-full lg:w-[20%] md:h-full md:w-[20%] sm:h-full sm:w-[20%] belowSm:h-full belowSm:w-[20%]">
                                         <BsArrowUpRightCircleFill className="text-5xl text-amber-400" />
                                     </div>
                                 </div>
@@ -58,8 +107,14 @@ export default function Footer() {
                         </div>
                     </div>
                 </div>
-                <div className="h-[45vh] w-[90%] mx-auto flex justify-center p-2">
-                    <div className="h-full w-[20%] text-base font-base text-gray1">
+                <div className="lg:h-[45vh] lg:w-[90%] lg:mx-auto lg:flex lg:justify-center lg:p-2
+                                md:h-[45vh] md:w-[90%] md:mx-auto md:flex md:justify-center md:p-2
+                                sm:h-[100vh] sm:w-[90%] sm:mx-auto sm:flex sm:justify-center sm:flex-wrap
+                                belowSm:h-[100vh] belowSm:w-[90%] belowSm:mx-auto belowSm:flex belowSm:justify-center belowSm:p-2 belowSm:flex-wrap">
+                    <div className="lg:h-full lg:w-[20%]
+                                    md:h-full md:w-[20%]
+                                    sm:h-[40vh] sm:w-[50%]
+                                    belowSm:h-[40vh] belowSm:w-[50%] text-base font-base text-gray1">
                         <p className="text-xl text-cream my-4 font-medium">Home</p>
                         <p className="my-2">Feature</p>
                         <p className="my-2">Blog</p>
@@ -68,7 +123,10 @@ export default function Footer() {
                         <p className="my-2">Contact us</p>
                         <p className="my-2">Newsletter</p>
                     </div>
-                    <div className="h-full w-[20%] text-base font-base text-gray1">
+                    <div className="lg:h-full lg:w-[20%]
+                                    md:h-full md:w-[20%]
+                                    sm:h-[40vh] sm:w-[50%]
+                                    belowSm:h-[40vh] belowSm:w-[50%] text-base font-base text-gray1">
                         <p className="text-xl text-cream my-4 font-medium">News</p>
                         <p className="my-2">Trending Stories</p>
                         <p className="my-2">Featured Videos</p>
@@ -77,7 +135,10 @@ export default function Footer() {
                         <p className="my-2">Politics</p>
                         <p className="my-2">Enviroment</p>
                     </div>
-                    <div className="h-full w-[20%] text-base font-base text-gray1">
+                    <div className="lg:h-full lg:w-[20%]
+                                    md:h-full md:w-[20%]
+                                    sm:h-[40vh] sm:w-[50%]
+                                    belowSm:h-[40vh] belowSm:w-[50%] text-base font-base text-gray1">
                         <p className="text-xl text-cream my-4 font-medium">News</p>
                         <p className="my-2">Quantum Computing</p>
                         <p className="my-2">Al Ethics</p>
@@ -86,7 +147,10 @@ export default function Footer() {
                         <p className="my-2">Renewable Energy</p>
                         <p className="my-2">Biohacking</p>
                     </div>
-                    <div className="h-full w-[20%] text-base font-base text-gray1">
+                    <div className="lg:h-full lg:w-[20%]
+                                    md:h-full md:w-[20%]
+                                    sm:h-[40vh] sm:w-[50%]
+                                    belowSm:h-[40vh] belowSm:w-[50%] text-base font-base text-gray1">
                         <p className="text-xl text-cream my-4 font-medium">News</p>
                         <p className="my-2">AI Revolution</p>
                         <p className="my-2">Blog</p>
@@ -94,18 +158,23 @@ export default function Footer() {
                         <p className="my-2">AI Conversation</p>
 
                     </div>
-                    <div className="h-full w-[20%] text-base font-base text-gray1">
-                        <p className="text-xl text-gray-400 my-2 font-medium">News</p>
-                        <p>Feature</p>
-                        <p>Blog</p>
-                        <p>Resource</p>
-                        <p>Testimonials</p>
-                        <p>Contact us</p>
-                        <p>Newsletter</p>
+                    <div className="lg:h-full lg:w-[20%]
+                                    md:h-full md:w-[20%]
+                                    sm:h-[20vh] sm:w-[90%] sm:mx-auto
+                                    belowSm:h-[20vh] belowSm:w-[90%] belowSm:mx-auto text-base font-base text-gray1">
+                        <p className="text-xl text-gray-400 my-2 font-medium">Newsletters</p>
+                        <div className="h-[8vh] w-full rounded-xl flex justify-center items-center border border-gray-800">
+                            <input className="h-full w-[60%] bg-dark1 text-white rounded-l-xl p-1"/>
+                            <button className="h-full w-[40%] bg-amber-400 text-dark1 rounded-r-xl text-xl font-medium">subscribe</button>
+                        </div>
+                        
                     </div>
                 </div>
                 <div className="h-0 w-[90%] mx-auto border-2 border-gray-800 rounded-xl" />
-                <div className="h-[5vh] w-[90%] mx-auto flex justify-center p-2">
+                <div className="lg:h-[5vh] lg:w-[90%] lg:mx-auto lg:flex lg:justify-center lg:p-2
+                                md:h-[5vh] md:w-[90%] md:mx-auto md:flex md:justify-center md:p-2
+                                sm:h-[10vh] sm:w-[90%] sm:mx-auto sm:flex sm:justify-center sm:p-2
+                                belowSm:h-[10vh] belowSm:w-[90%] belowSm:mx-auto belowSm:flex belowSm:justify-center belowSm:p-2">
                     <div className="h-full w-[40%] text-start text-sm text-gray1 font-normal">
                         <p>Teams&Conditions<span>Privacy Policy</span></p>
                     </div>
