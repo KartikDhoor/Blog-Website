d81470f5f05ffcdcbba05acf531cbff292a05f2b
KartikDhoor <127188028+KartikDhoor@users.noreply.github.com>
2025-01-15 19:37:03 +0530

    updated the tailwind
    update tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,13 +8,16 @@ export default {
     extend: {
       colors: {
         pureblack: "#000000", // Single custom color
-        dark1:"#1E201E",
-        dark2:"#3C3D37",
-        gray1:"#4C585B",
-        cream:"#ECDFCC",
+        dark1: "#1E201E",
+        dark2: "#3C3D37",
+        gray1: "#4C585B",
+        cream: "#ECDFCC",
         customGreen: "#10b981", // Another custom color
         customGradientStart: "#ff7f50", // For gradient start
         customGradientEnd: "#ff4500",   // For gradient end
+      },
+      screens: {
+        sm: '480px', // Set to a smaller size
       },
     },
   },
