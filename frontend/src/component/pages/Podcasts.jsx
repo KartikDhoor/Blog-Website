a83b8017f1097ab52b4dbc197b91091d58775d98
KartikDhoor <127188028+KartikDhoor@users.noreply.github.com>
2025-01-20 16:11:30 +0530

    making podcast responsive

--- frontend/src/component/pages/Podcasts.jsx
+++ frontend/src/component/pages/Podcasts.jsx
@@ -36,9 +36,9 @@ export default function Podcasts() {
                             </div>
                         </div>
                     </div>
-                    <div className="h-[70vh] w-full border-y border-gray-800">
-                        <div className="h-full w-[90%] mx-auto flex justify-center">
-                            <div className="h-full w-[40%] border-r border-gray-800 flex items-center">
+                    <div className="h-[70vh] w-full border-y border-gray-800 md:h-[70vh] sm:h-auto belowSm:h-auto">
+                        <div className="h-full w-[90%] mx-auto md:flex md:justify-center">
+                            <div className="md:h-full md:w-[40%] md:border-r border-gray-800 flex items-center sm:w-full sm:border-b sm:h-[50vh] belowSm:h-[50vh] belowSm:border-b belowSm:w-full">
                                 <div className="h-auto w-[90%] mx-auto">
                                     <TbBulbFilled className="text-8xl text-amber-400" />
                                     <div className="h-auto w-full flex justify-between my-2">
@@ -66,24 +66,24 @@ export default function Podcasts() {
                                     </div>
                                 </div>
                             </div>
-                            <div className="h-full w-[60%] ">
+                            <div className="h-full md:w-[60%] sm:w-full belowSm:w-full">
                                 <div className="h-auto w-[90%] mx-auto">
                                     <div className="h-auto w-full p-4">
-                                        <img src="https://picsum.photos/1920/1080" className="h-[40vh] w-full rounded-xl" />
+                                        <img src="https://picsum.photos/1920/1080" className="md:h-[40vh] sm:h-[30vh] belowSm:h-[30vh] w-full rounded-xl" />
                                     </div>
                                     <div className="h-auto w-full text-white text-lg ">
                                         <p className="font-medium">Delves into the transformative impact of AI</p>
                                         <p className="text-sm text-gray1 line-clamp-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo laboriosam aliquam et quidem nemo doloribus temporibus, consequatur placeat? Iure corrupti esse nostrum at quis. Laudantium dicta natus aliquam quae exercitationem?</p>
-                                        <div className="h-auto w-full rounded-lg bg-pureblack border my-2 p-2 border-gray-800 flex justify-around gap-2">
-                                            <div className="h-full w-[33%] rounded-lg border border-gray-800 bg-dark1 p-2">
+                                        <div className="h-auto w-full rounded-lg bg-pureblack border my-2 p-2 border-gray-800 grid md:grid-cols-3 sm:grid-cols-1 gap-2">
+                                            <div className="h-full w-full rounded-lg border border-gray-800 bg-dark1 p-2">
                                                 <p className="text-sm text-gray1">Total Episode</p>
                                                 <p className="text-base text-white">50</p>
                                             </div>
-                                            <div className="h-full w-[33%] rounded-lg border border-gray-800 bg-dark1 p-2">
+                                            <div className="h-full w-full rounded-lg border border-gray-800 bg-dark1 p-2">
                                                 <p className="text-sm text-gray1">Average Episode Length</p>
                                                 <p className="text-base text-white">30 Min</p>
                                             </div>
-                                            <div className="h-full w-[33%] rounded-lg border border-gray-800 bg-dark1 p-2">
+                                            <div className="h-full w-full rounded-lg border border-gray-800 bg-dark1 p-2">
                                                 <p className="text-sm text-gray1">Release Frequence</p>
                                                 <p className="text-base text-white">Weekly</p>
                                             </div>
@@ -93,14 +93,14 @@ export default function Podcasts() {
                             </div>
                         </div>
                     </div>
-                    <div className="h-[70vh] w-full border-y border-gray-800">
-                        <div className="h-full w-[90%] mx-auto flex justify-center">
-                            <div className="h-full w-[40%] border-r border-gray-800 flex items-center">
+                    <div className="h-[70vh] w-full border-y border-gray-800 md:h-[70vh] sm:h-auto belowSm:h-auto">
+                        <div className="h-full w-[90%] mx-auto md:flex md:justify-center">
+                            <div className="md:h-full md:w-[40%] md:border-r border-gray-800 flex items-center sm:w-full sm:border-b sm:h-[50vh] belowSm:h-[50vh] belowSm:border-b belowSm:w-full">
                                 <div className="h-auto w-[90%] mx-auto">
                                     <GiConversation className="text-8xl text-amber-400" />
                                     <div className="h-auto w-full flex justify-between my-2">
                                         <div className="h-full w-[50%] text-3xl text-white font-medium">
-                                            <p>Ai Conversation</p>
+                                            <p>Ai Revelution</p>
                                         </div>
                                         <div className="h-full w-[50%] text-xl text-amber-400 flex justify-center items-center gap-2">
                                             <div className="rounded-full border border-gray-800 flex justify-center gap-2 p-2">
@@ -115,7 +115,7 @@ export default function Podcasts() {
                                     <div className="h-auto w-full rounded-lg border border-gray-800 flex justify-center">
                                         <div className="h-auto w-[50%] text-white p-2 ">
                                             <p className="text-sm text-gray1">Host</p>
-                                            <p>Mark Anderson</p>
+                                            <p>Dr.Sarah Mitchall</p>
                                         </div>
                                         <div className="h-auto w-[50%] p-2">
                                             <button className="h-full w-full flex justify-center border border-gray-800 items-center rounded-xl text-gray1 bg-dark1">Listen Podcast<LuArrowUpRight className="text-xl text-amber-400" /></button>
@@ -123,24 +123,24 @@ export default function Podcasts() {
                                     </div>
                                 </div>
                             </div>
-                            <div className="h-full w-[60%] ">
+                            <div className="h-full md:w-[60%] sm:w-full belowSm:w-full">
                                 <div className="h-auto w-[90%] mx-auto">
                                     <div className="h-auto w-full p-4">
-                                        <img src="https://picsum.photos/1920/1080" className="h-[40vh] w-full rounded-xl" />
+                                        <img src="https://picsum.photos/1920/1080" className="md:h-[40vh] sm:h-[30vh] belowSm:h-[30vh] w-full rounded-xl" />
                                     </div>
                                     <div className="h-auto w-full text-white text-lg ">
                                         <p className="font-medium">Delves into the transformative impact of AI</p>
                                         <p className="text-sm text-gray1 line-clamp-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo laboriosam aliquam et quidem nemo doloribus temporibus, consequatur placeat? Iure corrupti esse nostrum at quis. Laudantium dicta natus aliquam quae exercitationem?</p>
-                                        <div className="h-auto w-full rounded-lg bg-pureblack border my-2 p-2 border-gray-800 flex justify-around gap-2">
-                                            <div className="h-full w-[33%] rounded-lg border border-gray-800 bg-dark1 p-2">
+                                        <div className="h-auto w-full rounded-lg bg-pureblack border my-2 p-2 border-gray-800 grid md:grid-cols-3 sm:grid-cols-1 gap-2">
+                                            <div className="h-full w-full rounded-lg border border-gray-800 bg-dark1 p-2">
                                                 <p className="text-sm text-gray1">Total Episode</p>
                                                 <p className="text-base text-white">50</p>
                                             </div>
-                                            <div className="h-full w-[33%] rounded-lg border border-gray-800 bg-dark1 p-2">
+                                            <div className="h-full w-full rounded-lg border border-gray-800 bg-dark1 p-2">
                                                 <p className="text-sm text-gray1">Average Episode Length</p>
                                                 <p className="text-base text-white">30 Min</p>
                                             </div>
-                                            <div className="h-full w-[33%] rounded-lg border border-gray-800 bg-dark1 p-2">
+                                            <div className="h-full w-full rounded-lg border border-gray-800 bg-dark1 p-2">
                                                 <p className="text-sm text-gray1">Release Frequence</p>
                                                 <p className="text-base text-white">Weekly</p>
                                             </div>
@@ -163,9 +163,9 @@ export default function Podcasts() {
                         </div>
                     </div>
                     <div className="h-auto w-full">
-                    <div className="h-[50vh] w-full border-y border-gray-800">
-                        <div className="h-full w-[90%] mx-auto flex justify-center">
-                                <div className="h-full w-[30%] p-2">
+                    <div className="md:h-[50vh] md:w-full border-y border-gray-800 sm:h-auto belowSm:h-auto">
+                        <div className="h-full w-[90%] mx-auto grid md:grid-cols-3 sm:grid-cols1 gap-2">
+                                <div className="h-full w-full p-2">
                                     <div className="h-[30vh] w-full">
                                         <img src="https://picsum.photos/1920/1080" className="h-full w-full rounded"/>
                                     </div>
@@ -177,7 +177,7 @@ export default function Podcasts() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="h-full w-[30%] p-2 border-x border-gray-800">
+                                <div className="h-full w-full p-2 border-x border-gray-800">
                                 <div className="h-[30vh] w-full">
                                         <img src="https://picsum.photos/1920/1080" className="h-full w-full rounded"/>
                                     </div>
@@ -189,7 +189,7 @@ export default function Podcasts() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="h-full w-[30%] p-2">
+                                <div className="h-full w-full p-2">
                                 <div className="h-[30vh] w-full">
                                         <img src="https://picsum.photos/1920/1080" className="h-full w-full rounded"/>
                                     </div>
@@ -203,7 +203,7 @@ export default function Podcasts() {
                                 </div>
                             </div>
                         </div>
-                        <div className="h-[50vh] w-full border-y border-gray-800">
+                        <div className="h-[50vh] w-full border-y border-gray-800 md:block sm:hidden belowSm:hidden">
                         <div className="h-full w-[90%] mx-auto flex justify-center">
                                 <div className="h-full w-[30%] p-2">
                                     <div className="h-[30vh] w-full">
