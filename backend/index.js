03831f70f99cdb0eb9b83fdf65c51aea36681eea
KartikDhoor <127188028+KartikDhoor@users.noreply.github.com>
2025-12-26 08:53:51 +0530

    Update index.js
    just adding frontend

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,8 @@ app.use(express.json());
 // ✅ Updated CORS
 const allowedOrigins = [
   'http://localhost:5173', // local dev
-  'https://blog-website-to47.vercel.app' // deployed frontend
+  'https://blog-website-to47.vercel.app', // deployed frontend
+  'https://neuradhoor.kartikdhoor.com'
 ];
 
 app.use(cors({
